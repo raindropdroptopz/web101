fcc7f12638e59d6d3e6361b4eb78c6a816a68b4e
medusa <medusa@medusas-MacBook-Pro.local>
2025-11-24 11:43:03 +0700

    add more button

--- script.js
+++ script.js
@@ -1,3 +1,9 @@
 function showMessage() {
   alert("Hello VS Code");
 }
+
+const heading = document.querySelector("h1");
+
+function changeTitle() {
+  heading.textContent = "You've changed title!"
+}
